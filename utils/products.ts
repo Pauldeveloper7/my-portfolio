d555e0cce48908b38b4db9678fe90a210a4a3ffb
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-05-31 17:19:29 +0530

    animation improved

--- utils/products.ts
+++ utils/products.ts
@@ -26,60 +26,60 @@ export const products = [
       thumbnail:
         "/images/yoom.png",
     },
-    {
-      title: "Pixel Perfect",
+    // {
+    //   title: "Pixel Perfect",
       
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-    },
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+    // },
    
-    {
-      title: "Algochurn",
+    // {
+    //   title: "Algochurn",
       
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-    },
-    {
-      title: "Aceternity UI",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+    // },
+    // {
+    //   title: "Aceternity UI",
     
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-    },
-    {
-      title: "Tailwind Master Kit",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-    },
-    {
-      title: "SmartBridge",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-    },
-    {
-      title: "Renderwork Studio",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-    },
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+    // },
+    // {
+    //   title: "Tailwind Master Kit",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    // },
+    // {
+    //   title: "SmartBridge",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    // },
+    // {
+    //   title: "Renderwork Studio",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+    // },
    
-    {
-      title: "Creme Digital",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-    },
-    {
-      title: "Golden Bells Academy",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-    },
-    {
-      title: "Invoker Labs",
+    // {
+    //   title: "Creme Digital",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+    // },
+    // {
+    //   title: "Golden Bells Academy",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+    // },
+    // {
+    //   title: "Invoker Labs",
     
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-    },
-    {
-      title: "E Free Invoice",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-    },
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+    // },
+    // {
+    //   title: "E Free Invoice",
+    //   thumbnail:
+    //     "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+    // },
   ];
